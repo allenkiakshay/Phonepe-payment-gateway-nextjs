61ed57f8a48f5b47577c12deb9b144bece6238af
allenki_akshay <allenkiakshay8322@gmail.com>
2024-07-05 22:52:38 +0530

    chore: Update npm dependencies and add qrcode.react library
    This commit updates the npm dependencies in the `package.json` and `package-lock.json` files. It adds the `qrcode.react` library with version 3.1.0. This library is used to generate QR codes for payment requests.
    
    Please review and merge this commit.

--- src/app/qrpayment/page.jsx
+++ src/app/qrpayment/page.jsx
@@ -2,8 +2,11 @@
 
 import React from "react";
 import axios from "axios";
+import QRCode from "qrcode.react";
 
 export default function Page() {
+  const size = 128;
+  const [qrData, setQrData] = React.useState(null);
   const handleClick = async () => {
     try {
       const response = await axios.post("/api/payqrrequest", {
@@ -13,18 +16,20 @@ export default function Page() {
         amount: 100,
       });
 
-      window.open(
-        response.data.data.instrumentResponse.intentUrl,
-        "_blank"
-      );
+      setQrData(response.data.data.instrumentResponse.intentUrl);
     } catch (error) {
       console.error("Error making API request:", error);
     }
   };
 
   return (
-    <div>
+    <div className="flex flex-col justify-start items-start gap-40">
       <button onClick={handleClick}>Click me</button>
+      {qrData && <QRCode value={qrData} size={size} />}
+
+      <a href={qrData} target="_blank">
+        Pay Using UPI Apps
+      </a>
     </div>
   );
 }
